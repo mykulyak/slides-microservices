00d36484864fb3da3165a157eeef802ae1ad453b
Andriy Mykulyak <mykulyak@gmail.com>
2017-06-01 08:24:34 +0200

    Inicjalny komit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    transition: 'slide',
+    dependencies: []
+  });
+});
